171db61112a6d234f310126c0abed0d3769e8805
allgoody <allgoodiesnet@gmail.com>
2025-01-14 20:33:48 +0200

    added react to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon()
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), icon(), react()],
 });
